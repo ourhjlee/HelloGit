aea7099971d0a4b03835a13fce7df5235c9fc7d4
Yoonki Chang <spades@naver.com>
2018-05-18 00:27:43 +0900

    COMMENT 추가

--- Helo_Git/src/com/jds/main/HelloGit.java
+++ Helo_Git/src/com/jds/main/HelloGit.java
@@ -1,9 +1,13 @@
 package com.jds.main;
 
+/**
+ * @author hyukjoon
+ *
+ */
 public class HelloGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// PRINT
 		System.out.println("Hello Git!!!");
 	}
 
